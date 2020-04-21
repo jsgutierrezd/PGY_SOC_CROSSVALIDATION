957ed1e048c82b0a6b6de3137bb241f671c51825
jsgutierrezd <jsgutierrezd@unal.edu.co>
2020-04-21 15:58:20 -0500

    Plots

--- CrossValidationChecking.R
+++ CrossValidationChecking.R
@@ -38,11 +38,20 @@ descr <- function(columna){
   return(data.frame(MIN,Q1,MED,Q3,MAX,PROM,DESVEST,CV,CURT,SKEW))%>% round(digits=2)
 }
 descr(datrm$DA_0.30)
+# MIN   Q1  MED   Q3  MAX PROM DESVEST   CV CURT SKEW
+#1.19 1.49 1.52 1.54 1.61 1.51    0.05 3.59 7.01 -1.8
 descr(datrm$COS_0.30)
+# MIN  Q1 MED    Q3   MAX  PROM DESVEST    CV  CURT SKEW
+#0.88 7.8 9.4 12.65 61.55 11.01    5.63 51.12 15.23  2.6
 descr(datrm$COSkgm2) 
+# MIN  Q1  MED   Q3   MAX PROM DESVEST   CV  CURT SKEW
+#0.42 3.6 4.29 5.65 21.98 4.89    2.17 44.5 11.04 2.11
 descr(values(RK))
+# MIN   Q1  MED   Q3  MAX PROM DESVEST    CV CURT SKEW
+#1.95 4.05 4.32 4.77 8.41 4.52    0.71 15.74 5.21  1.3
 descr(values(RF))
-
+# MIN   Q1  MED   Q3  MAX PROM DESVEST    CV CURT SKEW
+#1.4 4.14 4.36 4.68 8.32 4.52    0.63 14.01 5.69 1.42
 
 #####Checking external cross validation#####
 
@@ -72,6 +81,7 @@ names(dat)
 summary(dat$RK)
 summary(dat$RF)
 
+#Validation measurements
 ecv <- function(sim,pred){
   ME <- me(sim,pred)#mean error
   MAE <- mae(sim,pred)#mean absolute error
@@ -94,5 +104,29 @@ ecv(dat$RF,dat$COSkgm2)
 #   ME  MAE  MSE  MSE   Q3   R2  AVE
 #-0.45 1.46 5.90 2.43 1.68 0.14 0.11
 
+
+
+#Updating plot and RF histogram
+x11()
+par(mar=c(5,5,5,0))
+par(mgp=c(3,1,0))
+hist(dat$COSkgm2*10, breaks = 100, main  = "Histogram of soc stock measured",
+     ylim=c(0,120),las=1,
+     xlab= expression("SOC stock" ~ (t~ha^{-1}))) 
+     
+x11()
+par(mar=c(5,5,5,0))
+par(mgp=c(4,1,0))
+hist(RK*10, breaks = 100, main  = "Histogram of soc stock estimated values by RK",
+     ylim=c(0,30000),las=1,
+     xlab= expression("SOC stock" ~ (t~ha^{-1})))
+     
+x11()
+par(mar=c(5,5,5,0))
+par(mgp=c(4,1,0))
+hist(RF*10, breaks = 100, main  = "Histogram of soc stock estimated values by RF",
+     ylim=c(0,35000),las=1,
+     xlab= expression("SOC stock" ~ (t~ha^{-1})))
+     
 
 
